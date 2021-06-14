3a243bceda1a257361b1b675c5ef1094f1893991
nihangveritas <nihang@veritastechnolabs.com>
2021-06-14 11:50:55 +0530

    Nihang Api Error Code

--- api/devicetype/devicetype.controller.js
+++ api/devicetype/devicetype.controller.js
@@ -4,76 +4,100 @@ module.exports = {
   createDevType: (req, res) => {
     const body = req.body;
     createDevType(body, (error, result) => {
-      if (error) {
-        return res.json({
-          success: 0,
-          message: error,
-        });
-      }
-      return res.json({
-        success: 1,
-        data: { devtypeid: result.devtypeid },
-      });
-    });
+      if(error){
+        return res.status(500).json({
+            "success":0,
+            "message":"Database connection error"
+        })
+    }
+    return res.status(200).json({
+        "success":1,
+        "data":{'Devicetype':results.insertId}
+    })
+})
   },
   getDevType: (req, res) => {
     getDevType((err, result) => {
-      if (err) {
-        return res.json({
-          success: 0,
-          message: err,
-        });
-      }
-
-      return res.json({
-        success: 1,
-        data: { result },
-      });
-    });
+      if(err){
+        return res.status(500).json({
+            success:0,
+            message:"Database connection error"
+        })
+    }
+    if(results.length == 0){
+        return res.status(404).json({
+            success:0,
+            message:"Record Doesn't Exist!!"
+        })
+    }
+    return res.status(200).json({
+        success:1,
+        data:{results}
+    })
+})
   },
   getDeveTypeById: (req, res) => {
     const devtypeid = req.params.devtypeid;
     getDeveTypeById(devtypeid, (err, result) => {
-      if (err) {
+      if(error){
         return res.json({
-          success: 0,
-          message: err,
-        });
-      }
-      return res.json({
-        success: 1,
-        data: { result },
-      });
-    });
+            "success":0,
+            "message":"error"
+        })
+    }
+    if(results.length == 0){
+        return res.status(404).json({
+            "success":0,
+            "message":"Record Doesn't Exist!!"
+        })
+    }
+    return res.json({
+        "success":1,
+        "data":{results}
+    })
+})
   },
   updateDevType: (req, res) => {
     const body = req.body;
     updateDevType(body, (err, result) => {
-      if (err) {
+      if(error){
         return res.json({
-          success: 0,
-          message: err,
-        });
-      }
-      return res.json({
-        success: 1,
-        message: "DeviceType Updated Successfully...",
-      });
-    });
+            "success":0,
+            "message":"Error"
+        })
+    }
+    if(results.affectedRows != 0){
+        return res.status(200).json({
+            success:1,
+            message:results.affectedRows+" "+'Device Type Updated Successfully!'
+        })
+    }
+
+    return res.status(404).json({
+        "success":1,
+        "message":"Record Doesn't Exist!!"
+    })
+})
   },
   deleteDevType: (req, res) => {
     const devtypeid = req.params.devtypeid;
     deleteDevType(devtypeid, (error, result) => {
-      if (err) {
+      if(error){
         return res.json({
-          success: 0,
-          message: err,
-        });
-      }
-      return res.json({
-        success: 1,
-        message: "DeviceType Deleted Successfully...",
-      });
-    });
+            "success":0,
+            "message":"error"
+        })
+    }
+    if(results.affectedRows != 0){
+        return res.status(200).json({
+            success:1,
+            message:result.affectedRows+" "+'Device Type Deleted Successfully!'
+        })
+    }
+    return res.status(404).json({
+        "success":0,
+        "message":"Record Doesn't Exist!!"
+    })
+})
   },
 };
--- api/wificred/wificred.controller.js
+++ api/wificred/wificred.controller.js
@@ -10,80 +10,105 @@ module.exports = {
     const body = req.body;
     console.log(body);
     createWifiCred(body, (err, result) => {
-      if (err) {
-        return res.json({
-          success: 0,
-          message: err,
-        });
-      }
-      return res.json({
-        success: 1,
-        data: { wificredid: result.insertId },
-      });
-    });
+      if(error){
+        return res.status(500).json({
+            "success":0,
+            "message":"Database connection error"
+        })
+    }
+    return res.status(200).json({
+        "success":1,
+        "data":{'WifiCred':results.insertId}
+    })
+})
   },
   
   getWifiCred: (req, res) => {
     getWifiCred((err, result) => {
-      if (err) {
-        return res.json({
-          success: 0,
-          message: err,
-        });
-      }
-      return res.json({
-        success: 1,
-        data: { result },
-      });
-    });
+      if(err){
+        return res.status(500).json({
+            success:0,
+            message:"Database connection error"
+        })
+    }
+    if(results.length == 0){
+        return res.status(404).json({
+            success:0,
+            message:"Record Doesn't Exist!!"
+        })
+    }
+    return res.status(200).json({
+        success:1,
+        data:{results}
+    })
+})
   },
 
   getWifiCredById: (req, res) => {
    const wificredid = req.params.wificredid;
     getWifiCredById(wificredid,(err, result) => {
-      if (err) {
+      if(error){
         return res.json({
-          success: 0,
-          message: err,
-        });
-      }
-      return res.json({
-        success: 1,
-        data: { result },
-      });
-    });
+            "success":0,
+            "message":"error"
+        })
+    }
+    if(results.length == 0){
+        return res.status(404).json({
+            "success":0,
+            "message":"Record Doesn't Exist!!"
+        })
+    }
+    return res.json({
+        "success":1,
+        "data":{results}
+    })
+})
   },
 
   wificredUpdate: (req, res) => {
     const body = req.body
     wificredUpdate(body,(err, result) => {
-      if (err) {
+      if(error){
         return res.json({
-          success: 0,
-          message: err,
-        });
-      }
-      return res.json({
-        success: 1,
-        message: "WifiCred Upadated successfully....",
-      });
-    });
+            "success":0,
+            "message":"Error"
+        })
+    }
+    if(results.affectedRows != 0){
+        return res.status(200).json({
+            success:1,
+            message:results.affectedRows+" "+'WifiCred Updated Successfully!'
+        })
+    }
+
+    return res.status(404).json({
+        "success":1,
+        "message":"Record Doesn't Exist!!"
+    })
+})
   },
 
   wificredDelete: (req, res) => {
     const wificredid = req.params.wificredid;
     wificredDelete(wificredid,(err, result) => {
-      if (err) {
+      if(error){
         return res.json({
-          success: 0,
-          message: err,
-        });
-      }
-      return res.json({
-        success: 1,
-        message: "WifiCred deleted successfully.....",
-      });
-    });
+            "success":0,
+            "message":"error"
+        })
+    }
+    if(results.affectedRows != 0){
+        return res.status(200).json({
+            success:1,
+            message:result.affectedRows+" "+'WifiCred Deleted Successfully!'
+        })
+    }
+    return res.status(404).json({
+        "success":0,
+        "message":"Record Doesn't Exist!!"
+    })
+})
   },
 };
 //insertId is fixed inbuild id we cant change it
