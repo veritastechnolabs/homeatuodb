c1d1b5390f76dd83154b2b66979cd21e1a52cb65
raviVeritas <ravi@veritastechnolabs.com>
2021-06-14 10:57:35 +0530

    bug: solve conflicts

--- app.js
+++ app.js
@@ -1,11 +1,3 @@
-<<<<<<< HEAD
-require("dotenv").config();
-const express = require("express");
-const cors = require("cors");
-const app = express();
-app.use(express.json());
-app.use(cors());
-=======
 const express= require('express');
 require("dotenv").config();
 const app= express();
@@ -14,7 +6,6 @@ app.use(express.json());
 app.use(cors());
 
 const aptrouter= require("./api/apartment/apartment.router");
->>>>>>> 19a78c063db61812721a1beddd5e749b7aab43fb
 const wificredrouter = require("./api/wificred/wificred.router");
 const manfdevicerouter= require("./api/manufacture/manuf.router");
 const userrouter= require("./api/user/user.router");
@@ -23,10 +14,7 @@ const roomrouter= require("./api/room/room.router");
 const devicerouter= require("./api/device/device.router");
 const apttyperouter= require("./api/apartmenttype/apartmenttype.router");
 
-<<<<<<< HEAD
-=======
 app.use("/api/apartment",aptrouter);
->>>>>>> 19a78c063db61812721a1beddd5e749b7aab43fb
 app.use("/api/wificred", wificredrouter);
 app.use("/api/manfdevice",manfdevicerouter)
 app.use("/api/user",userrouter);
@@ -34,19 +22,6 @@ app.use("/api/roomtype",roomtyperouter);
 app.use("/api/room",roomrouter);
 app.use("/api/device",devicerouter);
 app.use("/api/apttype",apttyperouter);
-<<<<<<< HEAD
-app.get("/api", (req, res) => {
-    console.log(req.body);
-  res.json({
-    success: 1,
-    message: "testing...",
-  });
-});
-
-app.listen(process.env.APP_PORT, () => {
-  console.log("connection done", process.env.APP_PORT);
-});
-=======
 
 app.get("/api",(req,res)=>{
     res.json({
@@ -58,4 +33,3 @@ app.get("/api",(req,res)=>{
 app.listen(process.env.APP_PORT,()=>{
     console.log("Server is running!!", process.env.APP_PORT);
 })
->>>>>>> 19a78c063db61812721a1beddd5e749b7aab43fb
